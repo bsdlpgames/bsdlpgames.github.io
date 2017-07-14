2f091264aad20632599aa6c079aa6bc12fcd6631
Jon Chen <bsd@voltaire.sh>
2017-07-14 10:37:26 -0700

    wip

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './components/header'
+
+class App extends Component {
+  render() {
+  return (
+      <body className='near-white bg-near-black roboto'>
+        <Header />
+      </body>
+    );
+  }
+}
+
+export default App;
