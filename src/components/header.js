d42939831c98bf3f10f829891e6cd15e21846c0c
Jon Chen <bsd@voltaire.sh>
2017-07-14 23:30:15 -0700

    set body class

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../logo.jpg'
 
 export default () => (
-  <nav className='pa3 pa4-ns mb4 near-white bg-near-black sans-serif'>
+  <nav className='pa3 pa4-ns mb4'>
     <img alt='avatar' className='br-100 mr3 h3 w3 dib v-mid' src={logo} />
     <a className='link b f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='/' title='bsdlp'>bsdlp</a>
     <a className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='https://twitch.tv/bsdlp' title='twitch'>twitch</a>
