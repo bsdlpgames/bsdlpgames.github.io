fde219cf1c3ad31e5fdbf85f1f3e1c0be77b2648
Jon Chen <bsd@voltaire.sh>
2017-07-15 00:15:18 -0700

    add about me on frontpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/header'
+import Hero from './components/hero'
 
 class App extends Component {
   render() {
   return (
-      <Header />
+      <div>
+        <Header />
+        <Hero />
+      </div>
     );
   }
 }
---
+++ src/components/hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <div className='center w-80 ph3-ns pv4'>
+    <div className='pl4 pl5-ns'>
+      <h1 className='normal'>FPS Player and Software Engineer</h1>
+    </div>
+  </div>
+)
