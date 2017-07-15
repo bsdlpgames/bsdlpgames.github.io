cc8d7624ffdb8913830ea38dcd85347d65f99ad2
Jon Chen <bsd@voltaire.sh>
2017-07-14 23:28:32 -0700

    use sans-serif font

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Header from './components/header'
 class App extends Component {
   render() {
   return (
-      <body className='near-white bg-near-black roboto'>
-        <Header />
-      </body>
+      <Header />
     );
   }
 }
