4eb569d8857d3c044beb53ddbf37dba0a236f6a1
Jon Chen <bsd@voltaire.sh>
2017-07-16 02:11:43 -0700

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import {
+  Router,
+  Route
+} from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 import Header from './components/header'
 import Hero from './components/hero'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Hero />
-      </div>
+      <Router history={createBrowserHistory()}>
+        <div>
+          <Header />
+          <Route exact path='/' component={Hero}/>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {
+  NavLink
+} from 'react-router-dom'
 import logo from '../logo.jpg'
 
 export default () => (
   <nav className='pa3 pa4-ns mb4'>
     <img alt='avatar' className='br-100 mr3 h3 w3 dib v-mid' src={logo} />
-    <a className='link b f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='/' title='bsdlp'>bsdlp</a>
+    <NavLink activeClassName='b' className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' to='/'>bsdlp</NavLink>
     <a className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='https://twitch.tv/bsdlp' title='twitch'>twitch</a>
     <a className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='https://twitter.com/bsdlp' title='twitter'>twitter</a>
   </nav>
