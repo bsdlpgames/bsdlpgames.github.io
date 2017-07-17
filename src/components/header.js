4dd1f2f12422982159805d537c02bce17bb7385b
Jon Chen <bsd@voltaire.sh>
2017-07-16 23:50:49 -0700

    update nav links

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,7 @@ import logo from '../logo.jpg'
 
 export default () => (
   <nav className='pa3 pa4-ns mb4'>
-    <img alt='avatar' className='br-100 mr3 h3 w3 dib v-mid' src={logo} />
-    <NavLink activeClassName='b' className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' to='/'>bsdlp</NavLink>
+    <NavLink className='link f6 f5-ns ttu near-white dib mr3' to='/'><img alt='avatar' className='br-100 h2 h3-ns w2 w3-ns dib v-mid' src={logo} /></NavLink>
     <NavLink activeClassName='b' className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' to='/schedule'>schedule</NavLink>
     <a className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='https://twitch.tv/bsdlp' title='twitch'>twitch</a>
     <a className='link f6 f5-ns ttu near-white hover-near-black hover-bg-near-white dib mr3' href='https://twitter.com/bsdlp' title='twitter'>twitter</a>
